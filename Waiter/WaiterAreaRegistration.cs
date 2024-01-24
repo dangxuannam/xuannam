7a34fe16009d65c690e6e5513bf11384ed01ef69
dangxuannam <155295095+dangxuannam@users.noreply.github.com>
2024-01-24 20:10:41 +0700

    Add files via upload

---
+++ Waiter/WaiterAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CafeOnline.Areas.Waiter
+{
+    public class WaiterAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Waiter";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Waiter_default",
+                "Waiter/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
